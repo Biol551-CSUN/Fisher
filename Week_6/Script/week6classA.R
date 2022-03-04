eb4cfafd4cddd1c261195091396f64049d8a473f
lindsayfisher22 <98852803+lindsayfisher22@users.noreply.github.com>
2022-03-03 17:30:36 -0800

    Commit Week_6

---
+++ Week_6/Script/week6classA.R
@@ -0,0 +1,11 @@
+###This is a script for class, I will practice with ###
+###Created by: Lindsay Fisher####
+### Created on: 2022-03-01###
+
+##Load Libraries##
+library(rmarkdown)
+library(tinytex)
+ 
+#Read in Data##
+
+
